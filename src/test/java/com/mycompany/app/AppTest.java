ae59e783568b5bdd0c57daceb003ddfcef96bcbe
hirosh7 <hirosh7@gmail.com>
2018-04-03 22:23:47 -0700

    updated test to add missing '!'

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello Wakanda" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello Wakanda!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello Wakanda!\"");
         }
